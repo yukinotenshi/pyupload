2581a60aa20f0288314fb0cd1ff0f05025961945
yukinotenshi <gabrielbentararaphael@gmail.com>
2018-12-05 22:12:33 +0700

    added keyword + description to pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='PyUpload',
-    version='0.1.3',
+    version='0.1.4',
     packages=find_packages(),
     author='yukinotenshi',
     author_email='gabriel.bentara@gmail.com',
@@ -13,6 +13,8 @@ setup(
         'requests-toolbelt',
         'click'
     ],
+    description='A simple CLI tool to upload file with direct link',
+    keywords='direct upload cli',
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     entry_points={
